086b1cd6f0c4cbbdeb28f44bb0eedaa903ce1e34
sumeet978 <searchsumeet@gmail.com>
2019-10-06 15:36:04 +0100

    Added routes to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   resources :users
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
