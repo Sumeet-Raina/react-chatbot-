ec128660241fa432228d38cecec28e66caccb0e0
sumeet978 <searchsumeet@gmail.com>
2019-10-06 15:37:45 +0100

    Added Stream Chat and Dialogflow integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def initialize
+    if ENV['STREAM_URL'].blank?
+      api_key = ENV['STREAM_API_KEY']
+      api_secret = ENV['STREAM_API_SECRET']
+    else
+      api_key, api_secret = ENV['STREAM_URL'][8..].split('@')[0].split(':')
+    end
+    @chat = StreamChat::Client.new(api_key=api_key, api_secret=api_secret)
+    @dialogflow = Google::Cloud::Dialogflow::Sessions.new
+  end 
+  
 end
