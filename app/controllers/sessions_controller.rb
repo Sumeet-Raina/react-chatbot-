31cdd2df495fa446208da51de3d4e1bb90de5078
sumeet978 <searchsumeet@gmail.com>
2019-10-06 14:18:20 +0100

    Added Session Controller for Logging In and Out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    user = User.find_by_handle(params[:handle])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      session[:chat_token] = @chat.create_token(user.handle)
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      flash.now[:alert] = 'Handle or password is invalid'
+      render 'new'
+    end
+  end
+
+  def create
+  end
+
+  def destroy
+    session[:user_id] = nil
+    session[:chat_token] = nil
+    redirect_to root_url, notice: 'Logged out!'
+  end
+end
