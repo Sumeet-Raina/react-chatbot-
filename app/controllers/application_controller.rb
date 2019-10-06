a63f909e1c474e1b8730ac21060a6162ea83b335
sumeet978 <searchsumeet@gmail.com>
2019-10-06 15:40:08 +0100

    Added chat index controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,13 @@ class ApplicationController < ActionController::Base
     @chat = StreamChat::Client.new(api_key=api_key, api_secret=api_secret)
     @dialogflow = Google::Cloud::Dialogflow::Sessions.new
   end 
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
   
 end
