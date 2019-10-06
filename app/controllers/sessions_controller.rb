66e4938d4e693b615b8a1da8dfbf1e22bad762be
sumeet978 <searchsumeet@gmail.com>
2019-10-06 15:32:17 +0100

    fixed method name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  def new
+  def create
     user = User.find_by_handle(params[:handle])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
@@ -11,8 +11,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  def create
-  end
 
   def destroy
     session[:user_id] = nil
